82af219c74a60dac58fb9eea3510f6067a9508a0
Temur <77517397+TemurBahri@users.noreply.github.com>
2022-07-05 21:47:22 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/timurbahri/bilgitesti/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.timurbahri.bilgitesti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import 'constrants.dart';
+
+void main() => runApp(BilgiTesti());
+
+class BilgiTesti extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.indigo[700],
+            body: SafeArea(
+                child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: SoruSayfasi(),
+            ))));
+  }
+}
+
+class SoruSayfasi extends StatefulWidget {
+  @override
+  _SoruSayfasiState createState() => _SoruSayfasiState();
+}
+
+class _SoruSayfasiState extends State<SoruSayfasi> {
+  List<Widget> secimler = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Bilgi Testi Soruları',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(children: secimler),
+        Expanded(
+          flex: 1,
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(12),
+                          textColor: Colors.white,
+                          color: Colors.red[400],
+                          child: Icon(
+                            Icons.thumb_down,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              secimler.add(kYanlisIcon);
+                            });
+                          },
+                        ))),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(12),
+                          textColor: Colors.white,
+                          color: Colors.green[400],
+                          child: Icon(Icons.thumb_up, size: 30.0),
+                          onPressed: () {
+                            setState(() {
+                              secimler.add(kDogruIcon);
+                            });
+                          },
+                        ))),
+              ])),
+        )
+      ],
+    );
+  }
+}
